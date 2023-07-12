3560b12e9f6fdaa652fd9307af8c0b0af21705cb
Iverkobs8 <kobepatalinghugiverson17@gmail.com>
2023-07-12 15:12:12 +0800

    wahahaahaha

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -7,44 +7,31 @@ import { useRouter } from "next/navigation";
 
 import useLoginModal from "@/app/hooks/useLoginModal";
 import useRegisterModal from "@/app/hooks/useRegisterModal";
-import useRentModal from "@/app/hooks/useRentModal";
-import { SafeUser } from "@/app/types";
 
 import MenuItem from "./MenuItem";
 import Avatar from "../Avatar";
 
-interface UserMenuProps {
-  currentUser?: SafeUser | null
-}
 
-const UserMenu: React.FC<UserMenuProps> = ({
-  currentUser
-}) => {
+
+const UserMenu  = () => {
+
   const router = useRouter();
 
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
-  const rentModal = useRentModal();
 
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = useCallback(() => {
     setIsOpen((value) => !value);
-  }, []);
+  }, [])
 
-  const onRent = useCallback(() => {
-    if (!currentUser) {
-      return loginModal.onOpen();
-    }
-
-    rentModal.onOpen();
-  }, [loginModal, rentModal, currentUser]);
 
   return ( 
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div 
-          onClick={onRent}
+          onClick={() => {}}
           className="
             hidden
             md:block
@@ -80,7 +67,6 @@ const UserMenu: React.FC<UserMenuProps> = ({
         >
           <AiOutlineMenu />
           <div className="hidden md:block">
-            <Avatar src={currentUser?.image} />
           </div>
         </div>
       </div>
@@ -100,35 +86,6 @@ const UserMenu: React.FC<UserMenuProps> = ({
           "
         >
           <div className="flex flex-col cursor-pointer">
-            {currentUser ? (
-              <>
-                <MenuItem 
-                  label="My trips" 
-                  onClick={() => router.push('/trips')}
-                />
-                <MenuItem 
-                  label="My favorites" 
-                  onClick={() => router.push('/favorites')}
-                />
-                <MenuItem 
-                  label="My reservations" 
-                  onClick={() => router.push('/reservations')}
-                />
-                <MenuItem 
-                  label="My properties" 
-                  onClick={() => router.push('/properties')}
-                />
-                <MenuItem 
-                  label="Airbnb your home" 
-                  onClick={rentModal.onOpen}
-                />
-                <hr />
-                <MenuItem 
-                  label="Logout" 
-                  onClick={() => signOut()}
-                />
-              </>
-            ) : (
               <>
                 <MenuItem 
                   label="Login" 
@@ -139,7 +96,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                   onClick={registerModal.onOpen}
                 />
               </>
-            )}
+         
           </div>
         </div>
       )}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,13 @@ import { Nunito } from 'next/font/google'
 import Navbar from '@/app/components/navbar/Navbar';
 import LoginModal from '@/app/components/modals/LoginModal';
 import RegisterModal from '@/app/components/modals/RegisterModal';
-import SearchModal from '@/app/components/modals/SearchModal';
-import RentModal from '@/app/components/modals/RentModal';
+
 
 import ToasterProvider from '@/app/providers/ToasterProvider';
 
 import './globals.css'
 import ClientOnly from './components/ClientOnly';
-import getCurrentUser from './actions/getCurrentUser';
+
 
 export const metadata = {
   title: 'Airbnb',
@@ -26,7 +25,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const currentUser = await getCurrentUser();
+
 
   return (
     <html lang="en">
@@ -35,9 +34,7 @@ export default async function RootLayout({
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <SearchModal />
-          <RentModal />
-          <Navbar currentUser={currentUser} />
+          <Navbar />
         </ClientOnly>
         <div className="pb-20 pt-28">
           {children}
